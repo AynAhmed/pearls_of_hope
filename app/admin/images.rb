17450c05c5be587fa8a6c45afe629322311b000e
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2024-01-17 11:51:54 -0600

    Set up resources for models for active admin, set carrierwave to upl oad images.

---
+++ app/admin/images.rb
@@ -0,0 +1,36 @@
+ActiveAdmin.register Image do
+    permit_params :title, :image_data
+  
+    index do
+      selectable_column
+      id_column
+      column :title
+      column :image_data
+      column :created_at
+      actions
+    end
+  
+    filter :title
+    filter :created_at
+  
+    form do |f|
+      f.inputs 'Image Details' do
+        f.input :title
+        f.input :image_data, as: :file
+      end
+      f.actions
+    end
+  
+    show do
+      attributes_table do
+        row :title
+        row :image do |image_record|
+          image_tag image_record.image_data.url if image_record.image_data.present?
+        end
+        row :created_at
+        row :updated_at
+      end
+      active_admin_comments
+    end
+  end
+  
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,5 @@
 class Video < ApplicationRecord
+    def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "id", "id_value", "title", "updated_at", "url"]
+      end
 end
