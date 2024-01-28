cf7219676275b38738188140a2170b990541505b
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2024-01-28 17:58:23 -0600

    Update: added content on teacher dashboard and styled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :courseworks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,12 +12,12 @@ class User < ApplicationRecord
          def self.ransackable_attributes(auth_object = nil)
           ["created_at", "email", "first_name", "id", "id_value", "last_name", "password", "role",  "phone_number", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
         end
-    
+
         has_and_belongs_to_many :programs
         def full_name
           "#{first_name} #{last_name}"
         end
-      
+
         def stripe_attributes(pay_customer)
           {
             address: {
