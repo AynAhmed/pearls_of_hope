93c433e5b2210301b97677449eb83a90ffe83549
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2024-01-30 15:49:49 -0600

    Update: extra condition to nav to handle user who are not login

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,28 +1,33 @@
 class ProgramsController < ApplicationController
     before_action :set_program, only: [:show, :edit, :update, :destroy, :signup]
-  
+
+
     def index
       @programs = Program.all
     end
-  
+
     def show
+
     end
+
     def new
         @program = Program.new
-      end
-    
+    end
+
     def create
         @program = Program.new(program_params)
-    
+
     if @program.save
         redirect_to @program, notice: 'Program was successfully created.'
     else
         render :new
     end
+
     end
+
     def edit
     end
-  
+
     def update
       if @program.update(program_params)
         redirect_to @program, notice: 'Program was successfully updated.'
@@ -40,13 +45,17 @@ class ProgramsController < ApplicationController
         @program.destroy
         redirect_to programs_url, notice: 'Program was successfully destroyed.'
     end
-      
-  
+
+
     private
-  
+
     def set_program
-      @program = Program.find_by(name: params[:id])
+      @program = Program.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      # Handle the case where the program is not found
+      redirect_to programs_path, alert: 'Program not found'
     end
+
   
     def program_params
       params.require(:program).permit(:name, :description, :program_type, :age_group, :date)
