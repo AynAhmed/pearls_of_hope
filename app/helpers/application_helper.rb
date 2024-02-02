290634023a521ac9b234359dca4aff445220644e
Ridwan Dahir <ridwandahir@Ridwans-MacBook-Air-2.local>
2024-02-02 08:14:37 -0600

    I added a method to seed.rb for the program images and associated with a helper and updated the index/program with its styling to make it look modern

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
-end
+    def program_image_path(program)
+      case program.name
+      when "Al-Mustaqbal Academy"
+        asset_path("al_mustaqbal_academy.jpg")
+      when "Charms"
+        asset_path("charms.jpg")
+      when "Climb"
+        asset_path("climb.jpg")
+      when "Diamonds"
+        asset_path("diamonds.jpg")
+    when "Explorers"
+        asset_path("explorers.jpg")
+    when "Frontiers"
+        asset_path("frontiers.jpg")
+    when "Pearls"
+        asset_path("pearls.jpg")
+    when "Jewels"
+        asset_path("jewels.jpg")
+      else
+        # Return a placeholder image path if specified image is not found
+        asset_path("poh.jpg")
+      end
+    end
+  end
