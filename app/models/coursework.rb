751c0c73cd894840d7d8fb0cd32950977cb98a24
Ridwan Dahir <ridwandahir@Ridwans-MacBook-Air-2.local>
2024-02-08 13:20:15 -0600

    added courseworks onto active admin

--- app/models/coursework.rb
+++ app/models/coursework.rb
@@ -1,4 +1,12 @@
 class Coursework < ApplicationRecord
   belongs_to :program
   has_many_attached :attachments
+
+  def self.ransackable_associations(auth_object = nil)
+    ["program"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "id_value", "name", "program_id", "updated_at", "user_id"]
+  end
 end
